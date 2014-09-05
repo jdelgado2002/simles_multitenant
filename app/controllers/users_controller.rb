7d5fbf1e94ee92b4b88f74c0ef1dfa5adfb73915
jose mauricio delgado <jdelgado2002@gmail.com>
2014-09-05 13:25:12 -0500

    adding debug info when developing. adding routes to the user REST methods. creating initial users view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+  def show
+    #grabs the id paramter from the url /users/id and attempts to search for a user by id.
+    @user = User.find(params[:id])
+  end
+
   def new
   end
 end
